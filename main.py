7cfd3888a0d2824a7187925b42da5b4cabed38f0
Blah029 <81973454+Blah029@users.noreply.github.com>
2022-10-29 19:51:38 +0530

    Update docstring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """EE356: Electronic Product Design Project
 
+A portable teaching aid that can be used to teach basic arithmetic
+operations to primary school children ages 6-8 years old.
+
 Group 10
     E/17/146: Jayawickrama JPD
     E/17/234: Pandukabhaya VKM
