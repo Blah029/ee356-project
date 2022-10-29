7482ad5f0d6a774710ca1533943fbcf8e64fc712
Blah029 <81973454+Blah029@users.noreply.github.com>
2022-10-29 19:36:57 +0530

    Doctring and general template

---
+++ main.py
@@ -0,0 +1,13 @@
+"""EE356: Electronic Product Design Project
+
+Group 10
+    E/17/146: Jayawickrama JPD
+    E/17/234: Pandukabhaya VKM
+    E/17/371: Warnakulasuriya R
+"""
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
