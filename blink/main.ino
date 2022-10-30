5e90ad2d7ce94eec23261a7aed3c320cc08a2e3d
Blah029 <81973454+Blah029@users.noreply.github.com>
2022-10-30 16:08:34 +0530

    Create code template

---
+++ blink/main.ino
@@ -0,0 +1,18 @@
+/*EE356: Electronic Product Design project
+
+A portable teaching aid that can be used to teach basic arithmetic
+operations to primary school children ages 6-8 years old.
+
+E/17/146 Jayawickrama J. P. D.
+E/17/234 Pandukabhaya V. K. M.
+E/17/371 Warnakulasuriya R.
+*/
+
+
+// Set up I/O pins
+void setup() {
+}
+
+// Main loop
+void loop() {
+}
