ef2010153ace704ab31b15f53d89a5b335fa6a5e
Blah029 <81973454+Blah029@users.noreply.github.com>
2022-11-01 11:31:52 +0530

    `More OCD stuff

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # ee356-project
-
 3rd year EE356: Electronic Product Design project
 
 - Group 10
--- arduino_main/arduino_main.ino
+++ arduino_main/arduino_main.ino
@@ -17,10 +17,10 @@ int op_in = 7;
 int disp_enable = 6;
 int disp_out = 5;
 int clk = 4;
-int prompts[40] = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 
-                   13, 14, 15, 16, 17, 18, 20, 21, 24, 25, 27, 28, 30, 
-                   32, 35, 36, 40, 42, 45, 48, 49, 54, 56, 63, 64, 72, 
-                   81};
+int prompts[40] = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 
+                   15, 16, 17, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 
+                   42, 45, 48, 49, 54, 56, 63, 64, 72, 81};
+
 
 // Set up I/O pins
 void setup() {
@@ -34,6 +34,7 @@ void setup() {
     pinMode(num2_in, INPUT);        // Operand 2 serial in
     pinMode(op_in, INPUT);          // Operator serial in
 }
+
 
 // Main loop
 void loop() {
